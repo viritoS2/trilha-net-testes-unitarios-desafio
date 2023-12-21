25229cded1dd00d0c25b678602fc6d77d749cbfa
viritoS2 <vitoramorimof@gmail.com>
2023-12-21 20:25:43 -0300

    removendo comentários do teste de strings

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -9,65 +9,46 @@ public class ValidacoesStringTests
     [Fact]
     public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
     {
-        //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
-
-        // Arrange
         var texto = "OlaAbc";
         var resultadoEsperado = 6;
 
-        // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
 
-        // Assert
         Assert.Equal(resultadoEsperado, resultado);
     }
 
     [Fact]
     public void DeveContemAPalavraQualquerNoTexto()
     {
-        // Arrange
+
         var texto = "Esse é um texto qualquer";
         var textoProcurado = "qualquer";
 
-        //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
-        // Act
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
-        
 
-        // Assert
-        //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
         Assert.True(resultado);
     }
 
     [Fact]
     public void NaoDeveConterAPalavraTesteNoTexto()
     {
-        // Arrange
+
         var texto = "Esse é um texto qualquer";
         var textoProcurado = "teste";
 
-        // Act
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
-        // Assert
-        //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
         Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
     [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
-        //TODO: Corrigir a variável "textoProcurado" seção Arrange
-
-        // Arrange
         var texto = "Começo, meio e fim do texto procurado";
         var textoProcurado = "procurado";
 
-        // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
 
-        // Assert
         Assert.True(resultado);
     }
 }
